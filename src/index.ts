58e0c17b87e150dd0bf0d38ee7b8b5093edc1c11
wangwenlu <wangwenlu@kuaishou.com>
2022-03-21 19:33:07 +0800

    fix format

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,16 @@ import * as ts from 'typescript/lib/tsserverlibrary';
 import * as vscode from 'vscode';
 import * as path from 'path';
 
-function flatMap<T, U>(items: readonly T[], map: (v: T) => U | readonly U[]): U[] {
+function flatMap<T, U>(
+    items: readonly T[],
+    map: (v: T) => U | readonly U[]
+): U[] {
     const results: U[] = [];
     items.forEach(item => {
         const result = map(item);
         results.push(...(Array.isArray(result) ? result : [result]));
-    })
-    return results
+    });
+    return results;
 }
 
 interface TextCase {
@@ -18,10 +21,10 @@ interface TextCase {
 }
 
 class RunVitestCommand implements vscode.Command {
-    static ID = "vitest.runTest"
-    title = "Run";
+    static ID = 'vitest.runTest';
+    title = 'Run';
     command = RunVitestCommand.ID;
-    arguments?: [string, string]
+    arguments?: [string, string];
 
     constructor(text: string, filename: string) {
         this.arguments = [text, filename];
@@ -29,10 +32,10 @@ class RunVitestCommand implements vscode.Command {
 }
 
 class DevVitestCommand implements vscode.Command {
-    static ID = "vitest.devTest"
-    title = "Dev";
+    static ID = 'vitest.devTest';
+    title = 'Dev';
     command = DevVitestCommand.ID;
-    arguments?: [string, string]
+    arguments?: [string, string];
 
     constructor(text: string, filename: string) {
         this.arguments = [text, filename];
@@ -44,24 +47,35 @@ function runInTerminal(text: string, filename: string, cmd: 'run' | 'dev') {
 
     const terminal = vscode.window.createTerminal(`vitest - ${cmd}`);
 
-    const command = `cd ${JSON.stringify(casePath)} && npx vitest ${cmd} -t ${JSON.stringify(text)}`;
+    const command = `cd ${JSON.stringify(
+        casePath
+    )} && npx vitest ${cmd} -t ${JSON.stringify(text)}`;
     terminal.sendText(command, true);
     terminal.show();
 }
 
-vscode.commands.registerCommand(RunVitestCommand.ID, (text: string, filename: string) => {
-    runInTerminal(text, filename, 'run');
-})
+vscode.commands.registerCommand(
+    RunVitestCommand.ID,
+    (text: string, filename: string) => {
+        runInTerminal(text, filename, 'run');
+    }
+);
 
-vscode.commands.registerCommand(DevVitestCommand.ID, (text: string, filename: string) => {
-    runInTerminal(text, filename, 'dev');
-})
+vscode.commands.registerCommand(
+    DevVitestCommand.ID,
+    (text: string, filename: string) => {
+        runInTerminal(text, filename, 'dev');
+    }
+);
 
-const caseText = new Set(['it', 'describe'])
+const caseText = new Set(['it', 'describe']);
 
-function tryGetVitestTestCase(callExpression: ts.CallExpression, file: ts.SourceFile): TextCase | undefined {
+function tryGetVitestTestCase(
+    callExpression: ts.CallExpression,
+    file: ts.SourceFile
+): TextCase | undefined {
     if (!ts.isIdentifier(callExpression.expression)) {
-        return undefined
+        return undefined;
     }
 
     if (!caseText.has(callExpression.expression.text)) {
@@ -82,32 +96,39 @@ function tryGetVitestTestCase(callExpression: ts.CallExpression, file: ts.Source
         start: testName.getStart(file),
         end: testName.getEnd(),
         text: testName.text
-    }
+    };
 }
 
 class CodeLensProvider implements vscode.CodeLensProvider {
-    provideCodeLenses(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.ProviderResult<vscode.CodeLens[]> {
+    provideCodeLenses(
+        document: vscode.TextDocument,
+        token: vscode.CancellationToken
+    ): vscode.ProviderResult<vscode.CodeLens[]> {
         const text = document.getText();
-        const sourceFile = ts.createSourceFile('', text, ts.ScriptTarget.Latest);
+        const sourceFile = ts.createSourceFile(
+            '',
+            text,
+            ts.ScriptTarget.Latest
+        );
         const testCases: TextCase[] = [];
 
         visitor(sourceFile);
 
         return flatMap(testCases, x => {
-            const start = document.positionAt(x.start)
-            const end = document.positionAt(x.end)
+            const start = document.positionAt(x.start);
+            const end = document.positionAt(x.end);
 
             return [
                 new vscode.CodeLens(
                     new vscode.Range(start, end),
                     new RunVitestCommand(x.text, document.fileName)
-                ),
+                )
                 // new vscode.CodeLens(
                 //     new vscode.Range(start, end),
                 //     new DevVitestCommand(x.text, document.fileName)
                 // )
             ];
-        })
+        });
 
         function visitor(node: ts.Node) {
             if (token.isCancellationRequested) {
@@ -117,17 +138,19 @@ class CodeLensProvider implements vscode.CodeLensProvider {
             if (ts.isCallExpression(node)) {
                 const testCase = tryGetVitestTestCase(node, sourceFile);
                 if (testCase) {
-                    testCases.push(testCase)
+                    testCases.push(testCase);
                 }
             }
-            ts.forEachChild(node, visitor)
+            ts.forEachChild(node, visitor);
         }
     }
 }
 
 export function activate(context: vscode.ExtensionContext) {
-    context.subscriptions.push(vscode.languages.registerCodeLensProvider(
-        ['typescript', 'javascript'],
-        new CodeLensProvider()
-    ))
+    context.subscriptions.push(
+        vscode.languages.registerCodeLensProvider(
+            ['typescript', 'javascript'],
+            new CodeLensProvider()
+        )
+    );
 }
