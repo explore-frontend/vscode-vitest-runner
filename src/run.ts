f19c22602df60bc75588dcf825c816c635d7eb91
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2023-06-19 22:06:34 +0300

    feat: add support to monorepo

--- src/run.ts
+++ src/run.ts
@@ -1,39 +1,53 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
+import * as findUp from 'find-up';
+import { configFiles } from './vitest-config-files';
 
-function buildVitestArgs(text: string) {
-    return ['vitest', 'run', '-t', text];
+function getCwd(testFile: string) {
+    const configFilePath = findUp.sync(configFiles, { cwd: testFile });
+
+    if (!configFilePath) {
+        return;
+    }
+    return path.dirname(configFilePath);
 }
 
-function buildCdArgs(path: string) {
-    return ['cd', path];
+function buildVitestArgs({ caseName, casePath, sanitize = true }: { caseName: string, casePath: string, sanitize?: boolean }) {    
+    let sanitizedCasePath = casePath;
+    if(sanitize) {
+        sanitizedCasePath = JSON.stringify(casePath);
+        caseName = JSON.stringify(caseName);
+    }
+
+    const args = ['vitest', 'run', '-t', caseName, '--dir', sanitizedCasePath];
+
+    const rootDir = getCwd(casePath);
+    if(rootDir) {
+        args.push('--root', rootDir);
+    }
+
+    return args;
 }
 
 export function runInTerminal(text: string, filename: string) {
     const casePath = path.dirname(filename);
     const terminal = vscode.window.createTerminal(`vitest - ${text}`);
 
-    const casePathStr = JSON.stringify(casePath);
-    const caseNameStr = JSON.stringify(text);
-
-    const cdArgs = buildCdArgs(casePathStr);
-    terminal.sendText(cdArgs.join(' '), true);
-
-    const vitestArgs = buildVitestArgs(caseNameStr);
+    const vitestArgs = buildVitestArgs({ caseName: text, casePath: casePath });
     const npxArgs = ['npx', ...vitestArgs];
     terminal.sendText(npxArgs.join(' '), true);
     terminal.show();
 }
 
 function buildDebugConfig(
-    cwd: string,
+    casePath: string,
     text: string
 ): vscode.DebugConfiguration {
     return {
         name: 'Debug vitest case',
         request: 'launch',
-        runtimeArgs: buildVitestArgs(text),
-        cwd,
+        runtimeArgs: buildVitestArgs({ caseName: text, casePath: casePath, sanitize: false }),
+        cwd: getCwd(casePath) || casePath,
         runtimeExecutable: 'npx',
         skipFiles: ['<node_internals>/**'],
         type: 'pwa-node',
