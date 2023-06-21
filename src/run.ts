5ef541601f9afde970d5d1de82bb44a78f0e85c5
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2023-06-21 11:52:26 +0300

    fix opening new terminal every time

--- src/run.ts
+++ src/run.ts
@@ -12,9 +12,9 @@ function getCwd(testFile: string) {
     return path.dirname(configFilePath);
 }
 
-function buildVitestArgs({ caseName, casePath, sanitize = true }: { caseName: string, casePath: string, sanitize?: boolean }) {    
+function buildVitestArgs({ caseName, casePath, sanitize = true }: { caseName: string, casePath: string, sanitize?: boolean }) {
     let sanitizedCasePath = casePath;
-    if(sanitize) {
+    if (sanitize) {
         sanitizedCasePath = JSON.stringify(casePath);
         caseName = JSON.stringify(caseName);
     }
@@ -22,19 +22,32 @@ function buildVitestArgs({ caseName, casePath, sanitize = true }: { caseName: st
     const args = ['vitest', 'run', '-t', caseName, '--dir', sanitizedCasePath];
 
     const rootDir = getCwd(casePath);
-    if(rootDir) {
+    if (rootDir) {
         args.push('--root', rootDir);
     }
 
     return args;
 }
 
+let terminal: vscode.Terminal | undefined;
+
 export function runInTerminal(text: string, filename: string) {
     const casePath = path.dirname(filename);
-    const terminal = vscode.window.createTerminal(`vitest - ${text}`);
+    let terminalAlreadyExists = true;
+    if (!terminal || terminal.exitStatus) {
+        terminalAlreadyExists = false;
+        terminal?.dispose();
+        terminal = vscode.window.createTerminal(`vscode-vitest-runner`);
+    }
 
     const vitestArgs = buildVitestArgs({ caseName: text, casePath: casePath });
     const npxArgs = ['npx', ...vitestArgs];
+
+    if (terminalAlreadyExists) {
+        // CTRL-C to stop the previous run
+        terminal.sendText('\x03');
+    }
+
     terminal.sendText(npxArgs.join(' '), true);
     terminal.show();
 }
